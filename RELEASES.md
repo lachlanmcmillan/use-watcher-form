7abc4c312b37c214cf2671fc0080d23679ab63cd
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2025-07-30 14:28:37 +1000

    initial commit

---
+++ RELEASES.md
@@ -0,0 +1,8 @@
+## Releases 
+
+How to release
+
+- update CHANGELOG.md
+- update the version in package.json
+- bun run build
+- npm publish
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'example-build'
+  },
+  base: '/use-watcher-map/'
+})
