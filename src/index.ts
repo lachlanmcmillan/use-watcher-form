a64889aeea205e69170280f3d35f4078f1cf646f
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2025-07-31 10:13:57 +1000

    update build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello via Bun!');
+export type { PRecord, PRecordErrors, ValidationResult } from './types';
+
+export { useField } from './useField';
+
+export { useWatcherForm } from './useWatcherForm';
+export type { WatcherFormProps, WatcherForm } from './useWatcherForm';
+
+export { useWatcherFormCtx, WatcherFormCtx } from './WatcherFormCtx';
+
+export { WatcherFormDebugger } from './WatcherFormDebugger';
+
+export { WatcherFormProvider } from './WatcherFormProvider';
